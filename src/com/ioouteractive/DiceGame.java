57e5730cf93969a0bf6459723d639b9e4b18e774
Thuy Pham <s246009@student.dtu.dk>
2024-10-31 11:35:41 +0100

    Update DiceGame.java
    importing scanner

--- src/com/ioouteractive/DiceGame.java
+++ src/com/ioouteractive/DiceGame.java
@@ -1,5 +1,6 @@
 package com.ioouteractive;
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
     private static final int[] fieldEffects = {0, 0, 250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
